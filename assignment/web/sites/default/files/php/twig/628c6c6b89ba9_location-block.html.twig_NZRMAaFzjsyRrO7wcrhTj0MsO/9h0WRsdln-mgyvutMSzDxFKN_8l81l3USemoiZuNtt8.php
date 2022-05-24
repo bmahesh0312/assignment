e04db2882f61694b951116cf8ba7ce4f4eeb44c7
bmahesh0312 <bmahesh03121@gmail.com>
2022-05-24 11:08:23 +0530

    Initial commit

---
+++ assignment/web/sites/default/files/php/twig/628c6c6b89ba9_location-block.html.twig_NZRMAaFzjsyRrO7wcrhTj0MsO/9h0WRsdln-mgyvutMSzDxFKN_8l81l3USemoiZuNtt8.php
@@ -0,0 +1,102 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* modules/specbee_admin/templates/location-block.html.twig */
+class __TwigTemplate_e44e61bd65352e6c28f556cedf08aa3479c83c727a6d7af327538c2dbbf0ceb3 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "<div>
+\tCountry : <p style=\"display:inline; text-align:right\">";
+        // line 2
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["country"] ?? null), 2, $this->source), "html", null, true);
+        echo "</p><br>
+\tCity : <p style=\"display:inline; text-align:right\">";
+        // line 3
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["city"] ?? null), 3, $this->source), "html", null, true);
+        echo "</p><br>
+\tTimestamp : <p style=\"display:inline; text-align:right\">";
+        // line 4
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["timestamp"] ?? null), 4, $this->source), "html", null, true);
+        echo "</p><br>
+</div>";
+    }
+
+    public function getTemplateName()
+    {
+        return "modules/specbee_admin/templates/location-block.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  50 => 4,  46 => 3,  42 => 2,  39 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "modules/specbee_admin/templates/location-block.html.twig", "C:\\xampp\\htdocs\\drupal\\assignment\\assignment\\web\\modules\\specbee_admin\\templates\\location-block.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array();
+        static $filters = array("escape" => 2);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
